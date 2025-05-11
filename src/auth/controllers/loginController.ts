c5bf088a265b5e4cc0b1b150c1f5dfa247dd06cc
alexsharich <alexsharich@gmail.com>
2025-05-12 00:41:07 +0300

    add: add helper function daysToMs

--- src/auth/controllers/loginController.ts
+++ src/auth/controllers/loginController.ts
@@ -1,14 +1,13 @@
 import {Request, Response} from 'express'
 import {authService} from "../service/auth-service";
 import {jwtServise} from "../../application/jwtService";
+import {daysToMs} from "../../helpers/daysToMs";
 
 export type LoginInputType = {
     loginOrEmail: string,
     password: string
 }
-const daysToMs = (days: number) => {
-    return days * 24 * 60 * 60 * 1000
-}
+
 export const loginController = async (req: Request<any, any, LoginInputType>, res: Response) => {
     const userId = await authService.loginWithEmailOrLogin(req.body)
     if (userId) {
--- src/auth/controllers/refreshTokenController.ts
+++ src/auth/controllers/refreshTokenController.ts
@@ -1,7 +1,25 @@
 import {Request, Response} from "express";
+import {authService} from "../service/auth-service";
+import {jwtServise} from "../../application/jwtService";
+import {daysToMs} from "../../helpers/daysToMs";
 
 export const refreshTokenController = (req: Request, res: Response) => {
-    /*Отправить рефрештокен в куки, и вернуть новую пару токенов*/
-    res.status(200).send({/*accessToken*/})
+    /*Отправить рефрештокен в куки, и вернуть новую пару токенов, старый рефреш положить в черный лист*/
+    const oldRefreshToken = req.cookies.refreshToken/* old refresh*/
+    const userId = req.userId
+    if (userId) {
+        const {accessToken, refreshToken} = jwtServise.createToken(userId)
+
+        /*Добавить старые токены в черный лист бд*/
+        /*Добавить новый токен в бд*/
+        /*Проверить нет ли новвых токенов в черном листе бд*/
+
+        res.status(200).json({accessToken})
+        res.cookie('refreshToken', refreshToken, {
+            maxAge: (daysToMs(3)),
+            httpOnly: true
+        })
+        return
+    }
     res.sendStatus(401)
 }
